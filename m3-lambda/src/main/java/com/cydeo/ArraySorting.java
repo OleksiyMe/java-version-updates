b3ba0b0c0e4af7b164aff03ba9f3b340301d7944
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-08-15 12:43:25 -0400

    comm

--- m3-lambda/src/main/java/com/cydeo/ArraySorting.java
+++ m3-lambda/src/main/java/com/cydeo/ArraySorting.java
@@ -1,4 +1,5 @@
 package com.cydeo;
+
 import java.lang.reflect.Array;
 
 public class ArraySorting {//quicksort, BubbleSort
@@ -6,9 +7,16 @@ public class ArraySorting {//quicksort, BubbleSort
     public static void main(String[] args) {
 
         ArraySorting as = new ArraySorting();
-        as.sort();
+
+        QuickSorting qs = new QuickSorting();
+        BubbleSort bs = new BubbleSort();
+        as.sort(bs);
+        as.sort(qs);
+
     }
 
-    private void sort(){
-        System.out.println("Quick Sorting array");
+    private void sort(Sorting sorting) {
+        sorting.sort();
+
     }
+}
