e4b4b6375f229aa213ea049a190db489f9e2875d
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-08-14 23:58:30 -0400

    comm

--- m2-enum/src/main/java/com/cydeo/CurrencyDemo.java
+++ m2-enum/src/main/java/com/cydeo/CurrencyDemo.java
@@ -4,6 +4,8 @@ public class CurrencyDemo {
 
     public static final CurrencyDemo PENNY = new CurrencyDemo();
     public static final CurrencyDemo NICKLE = new CurrencyDemo();
+
     public static final CurrencyDemo DIME= new CurrencyDemo();
     public static final CurrencyDemo QUARTER = new CurrencyDemo();
+
 }
