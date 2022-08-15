1e2e9a8b55371b643ae2dd029ce25082c0b839ad
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-08-15 15:44:34 -0400

    comm

--- m3-lambda/src/main/java/com/cydeo/ApplesByOzzy/AppleGreenCplorPredicate.java
+++ m3-lambda/src/main/java/com/cydeo/ApplesByOzzy/AppleGreenCplorPredicate.java
@@ -3,6 +3,6 @@ package com.cydeo.ApplesByOzzy;
 public class AppleGreenCplorPredicate implements ApplePredicate{
     @Override
     public boolean test(Apple apple) {
-        return Apple.;
+        return apple.getColor().equals(Color.GREEN);
     }
 }
--- m3-lambda/src/main/java/com/cydeo/ApplesByOzzy/AppleTest.java
+++ m3-lambda/src/main/java/com/cydeo/ApplesByOzzy/AppleTest.java
@@ -7,10 +7,25 @@ public class AppleTest {
     public static void main(String[] args) {
 
         List<Apple> inventory = new ArrayList<Apple>();
-        inventory.add(new Apple(300, Color.GREEN));
+        inventory.add(new Apple(300, Color.RED));
+        inventory.add(new Apple(50, Color.GREEN));
         inventory.add(new Apple(100, Color.RED));
         inventory.add(new Apple(200, Color.GREEN));
         inventory.add(new Apple(50, Color.RED));
+
+
+        List<Apple> heavyApple=filterApples(inventory, new AllpeHeavyPredicate());
+        System.out.println("heavyApple = " + heavyApple);
+        List<Apple> greenApple=filterApples(inventory, new AppleGreenCplorPredicate());
+        System.out.println("greenApple = " + greenApple);
+        System.out.println("========================================");
+
+       ApplePredicate weightApple = apple-> apple.getWeight()>200;
+
+       filterApples(inventory, weightApple);
+
+
+
 
     }
 
@@ -20,7 +35,7 @@ public class AppleTest {
 
         for (Apple apple : inventory) {
             if (applePredicate.test(apple)) {
-                result.add(apple)
+                result.add(apple);
             }
         }
         return result;
