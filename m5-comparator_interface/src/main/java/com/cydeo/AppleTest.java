ae19dd4dd4c18db2b59eedd45e48ad95421dd98e
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-08-18 15:51:43 -0400

    commit Stream start

--- m5-comparator_interface/src/main/java/com/cydeo/AppleTest.java
+++ m5-comparator_interface/src/main/java/com/cydeo/AppleTest.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+import static java.util.Comparator.comparing;
+
 public class AppleTest {
     public static void main(String[] args) {
 
 
-    List<Apple> inventory = new ArrayList<Apple>();
+        List<Apple> inventory = new ArrayList<Apple>();
         inventory.add(new Apple(300, Color.RED));
         inventory.add(new Apple(50, Color.GREEN));
         inventory.add(new Apple(100, Color.RED));
@@ -16,10 +18,25 @@ public class AppleTest {
         inventory.add(new Apple(50, Color.RED));
 
         inventory.forEach(e -> System.out.println(e));
+/*
+        Comparator<Apple> sortApple = comparing((Apple apple) -> apple.getWeight());
+        inventory.sort(sortApple);
+        System.out.println(inventory);*/
 
-        Comparator<Apple>  sortApple =Comparator.comparing((Apple apple)->
+
+        inventory.sort(comparing(Apple::getWeight));
+        System.out.println(inventory);
+
+        //Reverse
+        inventory.sort(comparing(Apple::getWeight).reversed());
+        System.out.println(inventory);
 
 
-}
+        inventory.sort(comparing(Apple::getWeight).reversed().thenComparing(Apple::getColor));
+
+
+
+
+    }
 
 }
