36ef3adf1ccd91cd5ac170c7caccfdf1cc9aff70
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-08-19 10:53:35 -0400

    commit Stream start

---
+++ m3-lambda/src/main/java/com/cydeo/killTheRabbit/Calculator.java
@@ -0,0 +1,40 @@
+package com.cydeo.killTheRabbit;
+
+import java.util.function.Function;
+import java.util.logging.SocketHandler;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        //example with a reference variable for lambda expression
+        Function<Double, Double> f1 = x -> Math.sqrt(x);
+        System.out.println("SQRT from 100 is " + calculate(100d, f1));
+        //example with a lambda expression as an argument
+        System.out.println("SQRT from 100 is " + calculate(100d, x -> Math.sqrt(x)));
+
+        //example with a reference variable for lambda expression
+        Function<Integer, Double> f2 = x -> Math.sin(x);
+        System.out.println("Sin from 30 is " + calculate(30, f2));
+        //example with a lambda expression as an argument
+        System.out.println("Sin from 30 is " + calculate(30, x -> Math.sin(x)));
+
+ //==============================================================================================
+        System.out.println("\nThe same with Method Reference");
+        //example with a reference variable for method reference
+        Function<Double, Double> f3 = Math::sqrt;
+        System.out.println("SQRT from 1000 is " + calculate(1000d, f3));
+        //example with a method reference as an argument
+        System.out.println("SQRT from 1000 is " + calculate(1000d, Math::sqrt));
+
+        //example with a reference variable for method reference
+        Function<Integer, Double> f4 = Math::cos;
+        System.out.println("Cos from 30 is " + calculate(30, f4));
+        //example with a method reference as an argument -- doesn't work
+        // System.out.println("Sin from 30 is "+calculate(30, Math::cos) );
+
+    }
+
+    private static <T extends Number, V> V calculate(T num, Function<T, V> f1) {
+        return f1.apply(num);
+    }
+}
--- m3-lambda/src/main/java/com/cydeo/killTheRabbit/KillTheRabbit.java
+++ m3-lambda/src/main/java/com/cydeo/killTheRabbit/KillTheRabbit.java
@@ -1,22 +1,39 @@
 package com.cydeo.killTheRabbit;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class KillTheRabbit {
     public static void main(String[] args) {
 
-        Rabbit r1=new Rabbit("Rabbit1",13,EyesColor.BROWN);
-        Rabbit r2=new Rabbit("Rabbit2",6,EyesColor.BLUE);
-        Rabbit r3=new Rabbit("Rabbit3",17,EyesColor.GREEN);
-        Rabbit r4=new Rabbit("Rabbit4",8,EyesColor.RED);
-        Rabbit r5=new Rabbit("Rabbit5",11,EyesColor.BLUE);
-        Rabbit r6=new Rabbit("Rabbit6",5,EyesColor.RED);
-        Rabbit r7=new Rabbit("Rabbit7",16,EyesColor.BLUE);
-        Rabbit r8=new Rabbit("Rabbit8",3,EyesColor.RED);
+        Rabbit r1 = new Rabbit("Rabbit1", 13, EyesColor.BROWN);
+        Rabbit r2 = new Rabbit("Rabbit2", 6, EyesColor.BLUE);
+        Rabbit r3 = new Rabbit("Rabbit3", 17, EyesColor.GREEN);
+        Rabbit r4 = new Rabbit("Rabbit4", 8, EyesColor.RED);
+        Rabbit r5 = new Rabbit("Rabbit5", 11, EyesColor.BLUE);
+        Rabbit r6 = new Rabbit("Rabbit6", 5, EyesColor.RED);
+        Rabbit r7 = new Rabbit("Rabbit7", 16, EyesColor.BLUE);
+        Rabbit r8 = new Rabbit("Rabbit8", 3, EyesColor.RED);
 
+        List<Rabbit> listOfRabbits = Arrays.asList(r1, r2, r3, r4, r5, r6, r7, r8);
 
+        Predicate<Rabbit> condition = rabbit ->
+                (rabbit.getEyeColor() == EyesColor.RED) && (rabbit.getWeight() == 5);
 
+        for (Rabbit eachRabbit : sortTheRabbits(listOfRabbits, condition)) {
+            System.out.println(eachRabbit);
+        }
 
+    }
 
+    private static List<Rabbit> sortTheRabbits(List<Rabbit> list, Predicate<Rabbit> p) {
+        List<Rabbit> result = new ArrayList<>();
 
-
+        for (Rabbit each : list) {
+            if (p.test(each)) result.add(each);
+        }
+        return result;
     }
 }
