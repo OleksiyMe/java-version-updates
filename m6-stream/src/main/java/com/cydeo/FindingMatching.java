114890d43322330335d5458354d41d026d398be9
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-08-21 11:50:23 -0400

    commit Finding And Matching

---
+++ m6-stream/src/main/java/com/cydeo/FindingMatching.java
@@ -0,0 +1,44 @@
+package com.cydeo;
+
+import com.cydeo.task.Dish;
+import com.cydeo.task.DishData;
+import com.cydeo.task.DishTask;
+
+import java.util.Optional;
+
+public class FindingMatching {
+
+
+    public static void main(String[] args) {
+
+        //ALL MATCH
+        System.out.println("//ALL MATCH");
+        boolean isHealthy = DishData.getAll().stream().allMatch(dish -> dish.getCalories() < 1000);
+        System.out.println(isHealthy);
+
+        //ANY MATCH
+        System.out.println("ANY MATCH");
+        if (DishData.getAll().stream().anyMatch(Dish::isVegetarian)) {
+            System.out.println("Menu is vegetarian friendly");
+        }
+
+        //NONE MATCH
+        System.out.println("NONE MATCH");
+        boolean isHealthy2 = DishData.getAll().stream().noneMatch(
+                dish -> dish.getCalories() >= 1000);
+
+        System.out.println(isHealthy2);
+
+        //FIND ANY
+        System.out.println("FIND ANY");
+      Optional<Dish> dish = DishData.getAll().stream().filter(Dish::isVegetarian).findAny();
+        System.out.println("dish = " + dish.get());
+
+
+
+
+
+    }
+
+
+}
