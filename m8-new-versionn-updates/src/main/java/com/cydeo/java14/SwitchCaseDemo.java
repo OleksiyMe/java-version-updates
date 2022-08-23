5868a5b0fab3890e63ad3e50ebdcb3da8e04bdfd
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-08-23 15:00:43 -0400

    java 11 -14

--- m8-new-versionn-updates/src/main/java/com/cydeo/java14/SwitchCaseDemo.java
+++ m8-new-versionn-updates/src/main/java/com/cydeo/java14/SwitchCaseDemo.java
@@ -1,5 +1,7 @@
 package com.cydeo.java14;
 
+import java.util.Scanner;
+
 public class SwitchCaseDemo {
     public static void main(String[] args) {
 
@@ -42,5 +44,24 @@ public class SwitchCaseDemo {
             default -> 0;
         };
 
+        int day2 = switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12 -> 31;
+            case 4, 6, 9 -> 30;
+            case 2 -> {
+                Scanner scan = new Scanner(System.in);
+                System.out.println("Enter year");
+                int year = scan.nextInt();
+                if (year % 4 == 0)
+                    yield 29;
+                else
+                    yield 28;
+
+            }
+
+        default -> 0;
     }
+
+    ;
+
+}
 }
