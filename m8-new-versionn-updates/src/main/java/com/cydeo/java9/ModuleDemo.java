41694741f17d11b16e89d8f1ebf81ef011a9feed
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-08-22 21:42:52 -0400

    commit Finding And Matching

--- m8-new-versionn-updates/src/main/java/com/cydeo/java9/ModuleDemo.java
+++ m8-new-versionn-updates/src/main/java/com/cydeo/java9/ModuleDemo.java
@@ -4,12 +4,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ModuleDemo {
-    public class ModuleDemo {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            System.out.println(String.class.getModule());
-            System.out.println(List.class.getModule());
-            System.out.println(Scanner.class.getModule());
-        }
+        System.out.println(String.class.getModule());
+        System.out.println(List.class.getModule());
+        System.out.println(Scanner.class.getModule());
     }
+}
