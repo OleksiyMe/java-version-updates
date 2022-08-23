315b4a0eefed79700fe51c40c7a5cd720edd39ce
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-08-22 21:42:23 -0400

    commit Finding And Matching

--- m8-new-versionn-updates/src/main/java/com/cydeo/java9/CompactStringDemo.java
+++ m8-new-versionn-updates/src/main/java/com/cydeo/java9/CompactStringDemo.java
@@ -5,10 +5,6 @@ public class CompactStringDemo {
     public static void main(String[] args) {
 
 
-
-
-        String abc="CYDEO";
+        String abc = "cydeo";
     }
-
-
 }
--- m8-new-versionn-updates/src/main/java/com/cydeo/java9/FactoryMethodsDemo.java
+++ m8-new-versionn-updates/src/main/java/com/cydeo/java9/FactoryMethodsDemo.java
@@ -5,23 +5,22 @@ import java.util.*;
 public class FactoryMethodsDemo {
     public static void main(String[] args) {
 
-
-        List<String> courses = Collections.unmodifiableList(Arrays.asList("Java", "Spring", "Agile"));
-
+        //Creating unmodifiable List before Java9
+        List<String> courses  = Collections.unmodifiableList(Arrays.asList("Java","Spring","Agile"));
+//        courses.add("TS");
         System.out.println(courses);
 
-        //Creating unmodifiable after java9
-        List<String> courses1 = List.of("Java", "Spring", "Agile");
+        //Creating unmodifiable List after Java9
+        List<String> myCourses = List.of("Java","Spring","Agile");
 
-        //Creating unmodifiable Set after java9
-        Set<String> myProducts = Set.of("Milk", "Bread", "Butter");
+        //Creating unmodifiable Set after Java9
+        Set<String> myProducts = Set.of("Milk","Bread","Butter");
 
-        //Creating unmodifiable Map after java9
-        Map<String, Integer> myCart = Map.ofEntries(
-                Map.entry("Samsung TV", 1),
-                Map.entry("PSP 5", 1),
-                Map.entry("Chair", 5)
-
+        //Creating unmodifiable Map after Java9
+        Map<String,Integer> myCart = Map.ofEntries(
+                Map.entry("Samsung TV",1),
+                Map.entry("PSP",1),
+                Map.entry("Chair",5)
         );
 
 
