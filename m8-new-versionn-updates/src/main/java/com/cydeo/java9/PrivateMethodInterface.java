152fe8a340a6ec9994368502cbbb90f18c6f5462
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-08-22 21:47:49 -0400

    java 9 and 10

--- m8-new-versionn-updates/src/main/java/com/cydeo/java9/PrivateMethodInterface.java
+++ m8-new-versionn-updates/src/main/java/com/cydeo/java9/PrivateMethodInterface.java
@@ -2,26 +2,25 @@ package com.cydeo.java9;
 
 import java.time.LocalDate;
 
-public class PrivateMethodInterface {
-/*
-        boolean isHoliday (LocalDate date);
+public interface PrivateMethodInterface {
 
-        default
-        boolean isBusinessDay (LocalDate date){
-            if (date.isBefore(LocalDate.of(2001, 1, 1))) {
-                throw new IllegalArgumentException();
-            }
-            return !isHoliday(date);
+    boolean isHoliday(LocalDate date);
+
+    default boolean isBusinessDay(LocalDate date){
+        validate(date);
+        return !isHoliday(date);
+    }
+
+    default LocalDate nextDay(LocalDate date){
+        validate(date);
+        LocalDate nextDate = date.plusDays(1);
+        return isHoliday(nextDate) ? nextDay(nextDate): nextDate;
+    }
+
+    private void validate(LocalDate date){
+        if(date.isBefore(LocalDate.of(2001,1,1))){
+            throw new IllegalArgumentException();
         }
-
-        default
-        LocalDate nextDay (LocalDate date){
-            if (date.isBefore(LocalDate.of(2001, 1, 1))) {
-                throw new IllegalArgumentException();
-            }
-            LocalDate nextDate = date.plusDays(1);
-            return isHoliday(nextDate) ? nextDay(nextDate) : nextDate;
-        }*/
-
+    }
 }
 
